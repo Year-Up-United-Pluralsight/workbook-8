d0d84fe788cf87d68368d8c7ec206854bb1af35d
Din Ganic <dinganic03@gmail.com>
2025-06-12 09:33:56 -0400

    Added pom.xml practice exercise 1

---
+++ LoggerExercise/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Log4j2 is working");
+        logMeLikeYouDo("☕");
+    }
+
+    private static void logMeLikeYouDo(String input) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("This is debug : " + input);
+        }
+
+        if (logger.isInfoEnabled()) {
+            logger.info("This is info : " + input);
+        }
+
+        logger.warn("This is warn : " + input);
+        logger.error("This is error : " + input);
+        logger.fatal("This is fatal : " + input);
+    }
+}
