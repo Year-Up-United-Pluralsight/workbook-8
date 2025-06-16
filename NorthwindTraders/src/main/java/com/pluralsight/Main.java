cd592c88d8cbe4a33dcf00a58517d021e1cfa512
Din Ganic <dinganic03@gmail.com>
2025-06-16 11:17:33 -0400

    Added exercise 2 - executing and displaying a query

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,46 +4,54 @@ import java.sql.Connection;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        try {
-
-            String username = args[0];
-            String password = args[1];
-
-            // load the MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 1. open a connection to the database
-            // use the database URL to point to the correct database
-            Connection connection;
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/northwind",
-                    username,
-                    password);
-
-            // create statement
-            // the statement is tied to the open connection
-            Statement statement = connection.createStatement();
-
-            // define your query
-            String query = "SELECT * FROM Products";
-
-            // 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
-
-            // process the results
-            while (results.next()) {
-                String product = results.getString("ProductName");
-                System.out.println(product);
-            }
-
-            // 3. Close the connection
-            connection.close();
+        // Ensures there is username and password
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.UsingDriverManager <username> <password>");
+            System.exit(1);
         }
 
-        catch (ClassNotFoundException | SQLException e){
-            e.printStackTrace();
+        // Gets username and password from command line args
+        String username = args[0];
+        String password = args[1];
+
+        // Loads the MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // 1. Opens Connection to Database (uses the database URL to point to the correct database)
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind", username, password);
+
+        // 2. Create preparedStatement for query
+        // the statement is tied to the open connection
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products"
+        );
+
+        // 3. Execute your query
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        // Print header
+        System.out.printf("%-11s %-35s %-11s %-14s%n", "ProductID", "ProductName", "UnitPrice", "UnitsInStock");
+        System.out.println("---------   ---------------------------------   ----------  ----------------");
+
+        // Loops through the results
+        while (resultSet.next()) {
+
+            int productId = resultSet.getInt("ProductID");
+            String productName = resultSet.getString("ProductName");
+            double unitPrice = resultSet.getDouble("UnitPrice");
+            int unitsInStock = resultSet.getInt("UnitsInStock");
+
+            System.out.printf("%-11s %-35s %-11s %-14s%n", productId, productName, unitPrice, unitsInStock);
         }
+
+        // 3. Closes the resources
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
     }
 }
